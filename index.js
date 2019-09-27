9731de56dea3ef0db1b75d6557ad5bbf0a1e88da
Gabriel Rufino <email@gabrielrufino.com>
2019-09-27 15:52:36 -0300

    Comando search pronto

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 require('dotenv').config()
 
+const axios = require('axios')
+const dayjs = require('dayjs')
 const Telegraf = require('telegraf')
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 bot.start(context => context.reply('Bem-vindo ao News Spy!'))
+
+bot.command('search', context => {
+  const subject = context
+                    .message
+                    .text
+                    .split(' ')
+                    .slice(1)
+                    .join('+')
+
+  axios.get(`https://newsapi.org/v2/top-headlines`, {
+    params: {
+      q: subject,
+      apiKey: process.env.NEWS_API_TOKEN,
+      from: dayjs().format('YYYY-MM-DD'),
+      to: dayjs().format('YYYY-MM-DD')
+    }
+  })
+    .then(({ data }) => {
+      const response = data.articles.map(news => `${news.title}\n${news.url}\n\n`)
+      context.reply(response.join(''))
+    })
+    .catch(error => {
+      console.log(error)
+    })
+})
 
 bot.launch()
   .then(() => {
