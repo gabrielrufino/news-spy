aa4e5a206a000383b59aa327610d9b15d22dd2c9
Gabriel Rufino <email@gabrielrufino.com>
2019-09-27 16:50:27 -0300

    Inicialização do database pronta

---
+++ init/db.js
@@ -0,0 +1,14 @@
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+
+const adapter = new FileSync('db.json')
+const db = low(adapter)
+
+db
+  .defaults({
+    subjects: [],
+    users: [],
+  })
+  .write()
+
+module.exports = db
