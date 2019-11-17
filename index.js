eec6aa73f51096a386ec315e2787f0b135abe641
Gabriel Rufino <contato@gabrielrufino.com>
2019-11-17 14:36:01 -0300

    Camada de middlewares

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const init = require('./init')
 
 const handlers = require('./src/handlers')
+const middlewares = require('./src/middlewares')
 const jobs = require('./src/jobs')
 
 init()
   .then(({ bot, repositories, services }) => {
+    middlewares({ bot, repositories })
     handlers({ bot, repositories, services })
     jobs({ repositories, bot, services })
 
