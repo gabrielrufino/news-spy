18b927acc3559d3874fe3fbf6bd1c0cf477f11e7
Gabriel Rufino <contato@gabrielrufino.com>
2019-11-17 13:09:51 -0300

    Repositório de admin

--- src/repositories/index.js
+++ src/repositories/index.js
@@ -1,7 +1,9 @@
+const admin = require('./admin')
 const subject = require('./subject')
 const user = require('./user')
 
 const repositories = db => ({
+  admin: admin(db),
   subject: subject(db),
   user: user(db)
 })
