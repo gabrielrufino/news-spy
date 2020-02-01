0e888a84bac1c77d3305474a57adabca24ec4fcd
Gabriel Rufino <contato@gabrielrufino.com>
2020-02-01 09:55:26 -0300

    Principal função do index.js movida para src/index.js

--- index.js
+++ index.js
@@ -1,19 +1,5 @@
+const app = require('./src')
 const init = require('./init')
 
-const handlers = require('./src/handlers')
-const jobs = require('./src/jobs')
-const middlewares = require('./src/middlewares')
-
 init()
-  .then(({ bot, repositories }) => {
-    const services = require('./src/services')
-
-    middlewares({ bot, repositories })
-    handlers({ bot, repositories, services })
-    jobs({ repositories, bot, services })
-
-    bot.launch()
-      .then(() => {
-        console.log('News Spy funcionando!')
-      })
-  })
+  .then(app)
