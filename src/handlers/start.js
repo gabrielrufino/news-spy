3c8d18932eef1bb9ba9d547388306084d11901e5
Gabriel Rufino <contato@gabrielrufino.com>
2020-02-03 11:32:25 -0300

    Nova mensagem de boas vindas

--- src/handlers/start.js
+++ src/handlers/start.js
@@ -1,7 +1,11 @@
-const start = ({ repositories }) => context => {
+const start = ({ repositories }) => async context => {
   repositories.user.createIfNotExists(context.from)
 
-  context.reply('Bem-vindo ao News Spy!')
+  const { first_name: firstName } = context.update.message.from
+
+  await context.reply(`Olá, ${firstName}! Seja bem-vindo ao News Spy. Eu serei seu espião! 🗞🕵`)
+  await context.reply('O meu trabalho é monitorar todas as notícias importantes sobre assuntos que te interessam')
+  await context.reply('Comece agora mesmo. Pense em um assunto e envie para mim o seguinte comando: "/spy [assunto]"')
 }
 
 module.exports = start
