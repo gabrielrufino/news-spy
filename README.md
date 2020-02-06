faf3941d229d4b04c8e8a6d513e3c0b79f76a6aa
Gabriel Rufino <contato@gabrielrufino.com>
2020-02-05 21:25:58 -0300

    monitorar => vigiar

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # news-spy
-Chatbot espião de notícias que monitora notícias de seu interesse e as envia em ordem de relevância
+Chatbot espião de notícias que vigia notícias de seu interesse e as envia em ordem de relevância
 
 ## Frequências possíveis
 
--- src/handlers/start.js
+++ src/handlers/start.js
@@ -4,7 +4,7 @@ const start = ({ repositories }) => async context => {
   const { first_name: firstName } = context.update.message.from
 
   await context.reply(`Olá, ${firstName}! Seja bem-vindo ao News Spy. Eu serei seu espião! 🗞🕵`)
-  await context.reply('O meu trabalho é monitorar todas as notícias importantes sobre assuntos que te interessam')
+  await context.reply('O meu trabalho é vigiar todas as notícias importantes sobre assuntos que te interessam')
   await context.reply('Comece agora mesmo. Pense em um assunto e envie para mim o seguinte comando: "/spy [assunto]"')
 }
 
