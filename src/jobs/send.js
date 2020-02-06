df7dde3448a0490491dce49c2ec28307dc1f2eb0
Gabriel Rufino <contato@gabrielrufino.com>
2020-02-05 23:06:05 -0300

    Enviando título e link da notícia

--- src/jobs/send.js
+++ src/jobs/send.js
@@ -7,7 +7,7 @@ const send = (userId, { repositories, bot }) => async () => {
     if (news) {
       const index = user.news.indexOf(news)
 
-      bot.telegram.sendMessage(user.telegram.id, news.url)
+      bot.telegram.sendMessage(user.telegram.id, `${news.title}\n\n${news.url}`)
       repositories.user.setNewsAsSent(userId, index)
     }
   } catch (error) {
