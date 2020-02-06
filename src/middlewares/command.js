4482aa420b36582de519f833588976b2ddf8e570
Gabriel Rufino <contato@gabrielrufino.com>
2020-02-06 20:29:34 -0300

    Correção do standard

--- src/middlewares/command.js
+++ src/middlewares/command.js
@@ -3,9 +3,9 @@ const removeCommand = require('../helpers/remove-command')
 
 const command = (context, next) => {
   const { updateSubTypes, message } = context
-  if (updateSubTypes.includes('text')) {    
+  if (updateSubTypes.includes('text')) {
     const { text } = message
-  
+
     if (text.startsWith('/')) {
       context.state.command = extractCommand(text)
       context.state.argument = removeCommand(text)
