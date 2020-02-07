7a635a48d6eadab9fc21082bacaec1d58678e956
Gabriel Rufino <contato@gabrielrufino.com>
2020-02-07 19:03:46 -0300

    Correção do standard

--- src/api/index.js
+++ src/api/index.js
@@ -2,15 +2,15 @@ const express = require('express')
 
 const api = () => {
   const app = express()
-  
+
   app.get('/', (_, response) => {
     response.json({
       alive: true
     })
   })
-  
+
   const { PORT } = process.env
-  
+
   app.listen(PORT, () => {
     console.log(`API is listening on ${PORT}`)
   })
