739a7e97d68be24f718192e377aa7e755edc8deb
Gabriel Rufino <contato@gabrielrufino.com>
2020-02-06 21:05:27 -0300

    Correções nas mensagens

--- src/jobs/invite-to-watch.js
+++ src/jobs/invite-to-watch.js
@@ -2,9 +2,9 @@ const inviteToWatch = ({ bot, repositories }) => async () => {
   const cursor = await repositories.user.getAll({ subjects: { $size: 0 } })
 
   cursor.forEach(async user => {
-    await bot.telegram.sendMessage(user.telegram.id, 'Identificamos que você não está vigiando nenhum assunto.')
-    await bot.telegram.sendMessage(user.telegram.id, 'Eu sou capaz de enviar notícias sobre assuntos que intessam a você em ordem de relevância.')
-    bot.telegram.sendMessage(user.telegram.id, 'Para vigiar um assunto me envie o comando "/vigiar [assunto]" ou envie o comando "/inativar" para eu que pare de incomodar.')
+    await bot.telegram.sendMessage(user.telegram.id, 'Identificamos que você não está vigiando nenhum assunto')
+    await bot.telegram.sendMessage(user.telegram.id, 'Eu sou capaz de enviar notícias sobre assuntos que intessam a você em ordem de relevância')
+    bot.telegram.sendMessage(user.telegram.id, 'Para vigiar um assunto me envie o comando "/vigiar [assunto]"')
   })
 }
 
