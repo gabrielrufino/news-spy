c190dc153134c8c536109a0a3e42fe53764fa794
Gabriel Rufino <contato@gabrielrufino.com>
2020-02-08 09:07:45 -0300

    Analisando sentimento do título da notícia e enviando a notícia com sentimento de maior magnitude

--- src/jobs/send.js
+++ src/jobs/send.js
@@ -1,11 +1,31 @@
-const send = (userId, { repositories, bot }) => async () => {
+const send = (userId, { bot, repositories, services }) => async () => {
   try {
     const user = await repositories.user.getById(userId)
 
-    const news = user.news.filter(({ sent }) => !sent)[0]
+    const allNews = user.news
+    const unsentNews = allNews.filter(({ sent }) => !sent)
 
-    if (news) {
-      const index = user.news.indexOf(news)
+    if (unsentNews.length > 0) {
+      const algorithmiaInput = unsentNews.map(news => ({
+        document: news.title,
+        language: 'auto'
+      }))
+
+      const { result: newsWithSentiment } = await services.algorithmia
+        .algo('nlp/SentimentAnalysis/1.0.5')
+        .pipe(algorithmiaInput)
+
+      newsWithSentiment.sort((newsA, newsB) => {
+        const sentimentA = Math.abs(newsA.sentiment)
+        const sentimentB = Math.abs(newsB.sentiment)
+  
+        return sentimentB - sentimentA
+      })
+
+      const mostImportantNews = newsWithSentiment[0]
+
+      const news = allNews.find(news => news.title === mostImportantNews.document)
+      const index = allNews.indexOf(news)
 
       bot.telegram.sendMessage(user.telegram.id, `${news.title}\n\n${news.url}`)
       repositories.user.setNewsAsSent(userId, index)
