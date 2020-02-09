1285393659450dc473995b06fc8d8645f9b8e8ec
Gabriel Rufino <contato@gabrielrufino.com>
2020-02-09 12:52:40 -0300

    Correção no middleware user-id

--- src/middlewares/user-id.js
+++ src/middlewares/user-id.js
@@ -1,10 +1,15 @@
 const userId = ({ repositories }) => async (context, next) => {
-  const { id } = context.message.from
+  const { message } = context.update
+  
+  if (message) {
+    const { id } = message.from
 
-  const user = await repositories.user.getByTelegramId(id)
-  if (user) {
-    context.state.user_id = user._id
+    const user = await repositories.user.getByTelegramId(id)
+    if (user) {
+      context.state.user_id = user._id
+    }
   }
+
 
   next()
 }
