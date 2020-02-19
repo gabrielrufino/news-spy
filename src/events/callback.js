410f919d8c62b93e6e7f0f98ca0a9aa2c94aa73e
Gabriel Rufino <contato@gabrielrufino.com>
2020-02-19 20:06:02 -0300

    Tratando alguns possíveis erros ao processar um feedback

--- src/events/callback.js
+++ src/events/callback.js
@@ -7,16 +7,19 @@ const callback = ({ repositories }) => async context => {
 
     const news = await repositories.user.getNewsById(user._id, payload.news_id)
 
-    const feedback = {
-      feedback: payload.feedback,
-      news
+    if (news) {
+      const feedback = {
+        feedback: payload.feedback,
+        news
+      }
+
+      await repositories.user.pushFeedback(user._id, feedback)
     }
 
-    await repositories.user.pushFeedback(user._id, feedback)
     context.editMessageReplyMarkup({ inline_keyboard: [] })
   } catch (error) {
     console.error(error)
-    context.reply('Tivemos um erro inesperado!')
+    context.reply('Tivemos um erro inesperado. Tente novamente mais tarde ou entre em contato com nosso suporte!')
   }
 }
 
