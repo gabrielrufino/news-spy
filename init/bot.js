8238cc42b231324096a338c14f1da0a398654f4a
Gabriel Rufino <contato@gabrielrufino.com>
2020-02-21 19:15:02 -0300

    Habilitando session no contexto

--- init/bot.js
+++ init/bot.js
@@ -1,7 +1,9 @@
 const Telegraf = require('telegraf')
+const session = require('telegraf/session')
 
 const { BOT_TOKEN } = process.env
 
 const bot = new Telegraf(BOT_TOKEN)
+bot.use(session())
 
 module.exports = bot
