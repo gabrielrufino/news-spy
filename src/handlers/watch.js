c2d71cef6622d969f330f39474f911b29a1d0fc8
Gabriel Rufino <contato@gabrielrufino.com>
2020-02-21 19:26:15 -0300

    Refatorando handler watch sem uso de switch/case

--- src/handlers/watch.js
+++ src/handlers/watch.js
@@ -1,21 +1,19 @@
 const watch = ({ repositories }) => context => {
   const { step } = context.session
 
-  switch (step) {
-    case 2:
-      const subject = context.message.text
+  if (!step) {
+    context.session.handler = 'watch'
+    context.session.step = 2
+    context.reply('Me envie o assunto que você deseja vigiar')
+  } else if (step === 2) {
+    const subject = context.message.text
 
-      const user = context.state.user
-      repositories.user.pushSubject(user._id, subject)
+    const user = context.state.user
+    repositories.user.pushSubject(user._id, subject)
 
-      context.reply(`Deixe comigo! Eu vou vigiar notícias relacionadas à ${subject} para você.`)
-      context.session.handler = undefined
-      context.session.step = undefined
-      break
-    default:
-      context.session.handler = 'watch'
-      context.session.step = 2
-      context.reply('Me envie o assunto que você deseja vigiar.')
+    context.reply(`Deixe comigo! Eu vou vigiar notícias relacionadas à ${subject} para você.`)
+    context.session.handler = undefined
+    context.session.step = undefined
   }
 }
 
