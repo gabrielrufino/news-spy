2833abeec14488e734f12b8df60b2e87144267ca
Gabriel Rufino <contato@gabrielrufino.com>
2020-02-24 20:57:40 -0300

    Limitando à 4 assuntos

--- src/handlers/watch.js
+++ src/handlers/watch.js
@@ -1,10 +1,20 @@
-const watch = ({ repositories }) => context => {
+const sleep = require('sleep-promise')
+
+const watch = ({ repositories }) => async context => {
   const { step } = context.session
 
   if (!step) {
-    context.session.handler = 'watch'
-    context.session.step = 2
-    context.reply('Me envie o assunto que você deseja vigiar')
+    const { subjects } = context.state.user
+
+    if (subjects.length < 4) {
+      context.session.handler = 'watch'
+      context.session.step = 2
+      context.reply('Que assunto você deseja vigiar?')
+    } else {
+      await context.reply('Você pode vigiar até 4 assuntos.')
+      await sleep(1000)
+      context.reply('Deixe de vigiar algum assunto para pode vigiar um outro assunto. Para isso, use o comando /deixar')
+    }
   } else if (step === 2) {
     const subject = context.message.text
 
