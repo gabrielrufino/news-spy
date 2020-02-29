d1045e19bed0b8e91f4c9a81867ff84ea018f284
Gabriel Rufino <contato@gabrielrufino.com>
2020-02-29 10:13:05 -0300

    Dois novos métodos no repositório de usuário: setar o usuário como ativo e setar o usuário como inativo

--- src/repositories/user.js
+++ src/repositories/user.js
@@ -164,6 +164,28 @@ module.exports = db => {
     }
   }
 
+  const setActive = async (id) => {
+    try {
+      await users.updateOne(
+        { _id: ObjectID(id) },
+        { $set: { active: true } }
+      )
+    } catch (error) {
+      throw new Error(error)
+    }
+  }
+
+  const setInactive = async (id) => {
+    try {
+      await users.updateOne(
+        { _id: ObjectID(id) },
+        { $set: { active: false } }
+      )
+    } catch (error) {
+      throw new Error(error)
+    }
+  }
+
   const setNewsAsSent = async (id, position) => {
     const key = `news.${position}.sent`
 
@@ -202,6 +224,8 @@ module.exports = db => {
     pushNews,
     pushSubject,
     removeSubject,
+    setActive,
+    setInactive,
     setNewsAsSent,
     updateField
   }
