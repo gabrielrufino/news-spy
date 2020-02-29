01194902db3eb24cf14d841202b566bf8c8fd63e
Gabriel Rufino <contato@gabrielrufino.com>
2020-02-29 11:11:43 -0300

    Correção no middleware de validação de usuário ativo ou não

--- src/middlewares/check-active.js
+++ src/middlewares/check-active.js
@@ -1,7 +1,7 @@
 const checkActive = (context, next) => {
   const { active } = context.state.user
 
-  if (active) {
+  if (active || (context.message.text && ['/ativar', '/desativar'].includes(context.message.text))) {
     next()
   } else {
     context.reply('Identificamos que você desativou os nossos serviços. Envie o comando /ativar para voltar')
