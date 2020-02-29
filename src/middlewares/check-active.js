b4400306ea378733587afe2665327655aada3df8
Gabriel Rufino <contato@gabrielrufino.com>
2020-02-29 10:44:12 -0300

    Middleware para checar se o usuário está ativo ou não

---
+++ src/middlewares/check-active.js
@@ -0,0 +1,11 @@
+const checkActive = (context, next) => {
+  const { active }  = context.state.user
+
+  if (active) {
+    next()
+  } else {
+    context.reply('Identificamos que você desativou os nossos serviços. Envie o comando /ativar para voltar')
+  }
+}
+
+module.exports = checkActive
